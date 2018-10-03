bebf2f173dc4615459c27521c1f842b8f43daab2
Anita <chaudhar2134@gmail.com>
2018-10-03 22:47:09 +0530

    first commit

---
+++ app/src/main/java/com/example/anu/edittext/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.anu.edittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                Toast message = Toast.makeText(getBaseContext(),str, Toast.LENGTH_LONG);
+                message.show();
+            }
+        });
+
+    }
+}
